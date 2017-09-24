ca3f39f8acc5b516eb6412bcf363002d3cb9f1d5
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-09-25 01:42:39 +0700

    Template literals strings

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 // console.log("Hello World! (From Webpack) with webpack-dev-server");
 
-// ==================== Variabel Scoping with let
+// ==================== Variabel Scoping with let ==========================
 
 // var a = 'Hello';
 // console.log(a);
@@ -8,7 +8,7 @@
 let a = 'Hello';
 // console.log(a);
 
-// dengan menggunakan let maka variabel yang diambil hanya dalam scope tanda kurung kurawal saja
+// === dengan menggunakan let maka variabel yang diambil hanya dalam scope tanda kurung kurawal saja
 {
     let a = 'goodbye';
     // console.log('a inside scope', a);
@@ -19,3 +19,18 @@ let a = 'Hello';
 }
 // Ini akan error karena variabellnya tidak akan terpanggil
 // console.log(salary);
+
+
+// ==================== Constanly scoping ==========================
+// === Const itu konstanta nilai yg udah diisi pertama ga boleh diganti. Musti tetep itu sampe akhir program
+
+// const a = 2;
+// === Ini akan menimbulkan error
+// a = 3 * 4;
+
+const array = [1, 2, 3];
+array.push(4);
+// console.log(array);
+
+// === Ini akan salah
+// array = [3]
