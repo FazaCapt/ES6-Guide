70900c29d07d21e53117c0ae643893ffb7d5fc21
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-09-25 11:26:37 +0700

    Operating and Destructing (Spreed, Arrays, Objects

--- app/index.js
+++ app/index.js
@@ -23,6 +23,7 @@
 
 // ==================== Constanly scoping ==========================
 // === Const itu konstanta nilai yg udah diisi pertama ga boleh diganti. Musti tetep itu sampe akhir program
+// === Kata kunci const menyatakan variabel yang tidak dapat ditetapkan ulang.
 
 // const a = 2;
 // === Ini akan menimbulkan error
@@ -37,12 +38,81 @@ array.push(4);
 
 // ==================== Template literals/string ==========================
 
-var a = 'Hello';
-var b = 'World';
-var c = a + ' ' + b;
+// var a = 'Hello';
+// var b = 'World';
+// var c = a + ' ' + b;
 
 // console.log(c);
 
 // === Ini bagian template string
-let d = `Hello ${b}`;
+// let d = `Hello ${b}`;
 // console.log(d);
+
+
+// ==================== Operating and Destructing ==========================
+
+// === Understanding the spread operator ===
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator
+
+// === Ini disebut spread karena '...a' mewakili 'let b'
+// let a = [7,8,9];
+// let b = [6, ...a, 10];
+
+// console.log(b);
+
+function print(a,b,c) {
+    console.log(a, b, c);
+}
+
+// let z = [1,4,3];
+// print(z[0], z[1], z[2]);
+// === es6 - Spreed
+// print(...z);
+
+function print(...y){
+    console.log(y);
+}
+// print(3,4,5,6,7);
+
+// ====================  Array: Destructing Assignment ==========================
+
+let c = [100, 200];
+// let a = c[0];
+// let b = c[1];
+// === Atau bisa juga
+let [a,b] = c;
+
+// console.log(a,b);
+
+let Boys = ['Dani', 'Jabar', 'Didik'];
+let [sari, suci, annis] = Boys;
+
+// console.log(sari, suci, annis);
+
+let x = [100, 200, 300, 400, 500, 600];
+let [y, ...z] = x;
+
+// console.log(y, z);
+
+// ==================== Object: Destructing Assignment  ==========================
+
+// let wizard = {magical: true, power: 10};
+// let magical = wizard.magical;
+// let power = wizard.power;
+// === Atau Bisa
+// let { magical, power} = wizard;
+
+// console.log(magical,power);
+
+// ==== Contoh 2
+
+let magical = true;
+let power = 34;
+
+let ranger = { magical: false, power: 9};
+// === Ini akan error bila berbarengan dengan let di atas.
+// let {magical , power} = ranger;
+// === Maka, harus seperti ini....
+({magical, power} = ranger);
+
+console.log(magical, power);
